07f4b655b0f90c63336876830cec7b5eaafbb0b2
c-santos <jcms121899@gmail.com>
2024-06-21 14:55:06 +0800

    added env in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: any) => {
+    const env = loadEnv(mode, process.cwd());
+
+    const PORT = parseInt(`${env.VITE_PORT ?? '3000'}`);
+
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            port: PORT,
+        },
+    });
+};
